9a77534eb178e3ee8ddfe872c94e9371963e2e5e
Rezwanul-Haque <rezwanul.cse@gmail.com>
2017-09-08 19:21:10 +0600

    Update file

--- Directory/www/views.py
+++ Directory/www/views.py
@@ -5,4 +5,4 @@ from .models import Person
 def index(request):
     people = Person.objects.all()
     
-    return render(request, 'index.html', {"people" : people})
+    return render(request, 'index.html', {'people' : people})
