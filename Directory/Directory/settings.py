5b1c71e8fc8efe7007b5d73dfa5c2cd9b2f4cbc3
Rezwanul-Haque <rezwanul.cse@gmail.com>
2017-09-08 18:12:02 +0600

    Updated setting and add Procfile and requirement so we can deploy to Heroku

--- Directory/Directory/settings.py
+++ Directory/Directory/settings.py
@@ -123,7 +123,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
-    BASE_DIR + '/www/static'
+    BASE_DIR + '/www/static',
 )
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManfestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
